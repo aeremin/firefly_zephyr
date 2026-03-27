fe8bba5f7ca19a38c52d4b7fcfaa4bdd5f93926f
Alexey Eremin <a.eremin.msu@gmail.com>
2026-03-27 23:22:11 +0100

    Fix EEPROM test

--- firmware/common/eeprom.h
+++ firmware/common/eeprom.h
@@ -13,9 +13,9 @@ const device* kEepromDevice = DEVICE_DT_GET(DT_ALIAS(eeprom));
 
 // Enables eeprom power. Without that reads/writes won't work.
 void EnablePower() {
-  #ifdef CONFIG_SOC_FAMILY_NRF
-  const gpio_dt_spec gpio_spec = GPIO_DT_SPEC_GET(DT_ALIAS(eeprom_en), gpios);
-  gpio_pin_configure_dt(&gpio_spec, GPIO_OUTPUT_ACTIVE);
+  #if DT_NODE_EXISTS(DT_ALIAS(eeprom_en))
+    const gpio_dt_spec gpio_spec = GPIO_DT_SPEC_GET(DT_ALIAS(eeprom_en), gpios);
+    gpio_pin_configure_dt(&gpio_spec, GPIO_OUTPUT_ACTIVE);
   #endif
 }
 
